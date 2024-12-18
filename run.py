1e7fc4e7665689eb7124255f339ffb289c1301ac
ander <ander4c9pi@gmail.com>
2024-12-18 00:31:49 -0500

    actualizacion

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 import os
 from flask import Flask
-from app.routes.system_routes import system_bp  # Importar el blueprint correctamente
+from app.routes.system_routes import system_bp  # Importa tus blueprints
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
 
-    # Registrar el blueprint con el prefijo "/api"
+    # Registrar blueprints
     app.register_blueprint(system_bp, url_prefix="/api")
-
     return app
 
 if __name__ == "__main__":
     app = create_app()
-    port = int(os.environ.get("PORT", 5000))  # Obtener el puerto dinámicamente de Render
-    app.run(host="0.0.0.0", port=port, debug=False)  # Configurar host y puerto
+    # Usa el puerto proporcionado por la variable de entorno `PORT` o 5000 como predeterminado
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
