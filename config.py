454f1ab4747793cd10a639d58145d3e7f52fe3e6
ander <ander4c9pi@gmail.com>
2024-12-19 11:09:58 -0500

    actualizacion2 api

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
-
--- run.py
+++ run.py
@@ -1,17 +1,6 @@
-import os
-from flask import Flask
-from app.routes.system_routes import system_bp  # Importa tus blueprints
+from app import create_app
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object("config.Config")
-
-    # Registrar blueprints
-    app.register_blueprint(system_bp, url_prefix="/api")
-    return app
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
-    # Usa el puerto proporcionado por la variable de entorno `PORT` o 5000 como predeterminado
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
